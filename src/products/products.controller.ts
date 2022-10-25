e35054aa4555dab5fa2f45c792e85f11b0e60183
Rafael Perdigão <dev.rafaelpermec@gmail.com>
2022-10-25 16:18:48 -0300

    :construction: feat: Stablished some product data model to the controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
   @Post()
-  addProduct(): any {
-    
+  addProduct(
+    @Body()
+    completeProduct: {
+      title: string;
+      description: string;
+      price: number;
+    },
+    // Another way to do it!:
+    // @Body('title') prodTitle: string,
+    // @Body('description') prodDescription: string,
+    // @Body('price') prodPrice: number,
+  ): any {
+    this.productsService.insertProduct(completeProduct);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './products.model';
 
 @Injectable()
 export class ProductsService {
-  x = 0;
+  products: Product[] = [];
+
+  insertProduct(prod: Omit<Product, 'id'>): Product[] {
+    const { title, price, description } = prod;
+    const newDate = Date.toString();
+    const newProduct = new Product(newDate, title, description, price);
+    this.products.push(newProduct);
+    return this.products;
+  }
 }
