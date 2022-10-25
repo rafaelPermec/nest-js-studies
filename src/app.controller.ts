e10464e968c92e4760fbd9ad9faeddb649af655c
Rafael Perdigão <dev.rafaelpermec@gmail.com>
2022-10-25 11:19:31 -0300

    :construction: feat: Assign post and delete endpoint to manipulate an simple array

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Content-Type', 'text-html')
   getHello(): string {
     return this.appService.getHello();
   }
@@ -13,5 +14,15 @@ export class AppController {
   @Get('/monocard/devs')
   getMono(): string[] {
     return this.appService.getMono();
+  }
+
+  @Post('/monocard/devs')
+  postMono(): string[] {
+    return this.appService.postMono();
+  }
+
+  @Delete('/monocard/devs')
+  deleteMono(): string[] {
+    return this.appService.deleteMono();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  devs = [
+    'Rafael',
+    'Russo',
+    'Igor',
+    'Stefferson',
+    'André',
+    'Gustavo',
+    'Perdigão',
+  ];
+
   getHello(): string {
     return 'Hello World!';
   }
+
   getMono(): string[] {
-    return [
-      'Rafael',
-      'Russo',
-      'Igor',
-      'Stefferson',
-      'André',
-      'Gustavo',
-      'Perdigão',
-    ];
+    return this.devs;
+  }
+
+  postMono(newDev: string): string[] {
+    this.devs.push(newDev);
+    return this.devs;
+  }
+
+  deleteMono(delDev: string): string[] {
+    const deleteDev = this.devs.filter((item) => item !== delDev);
+    return deleteDev;
   }
 }
